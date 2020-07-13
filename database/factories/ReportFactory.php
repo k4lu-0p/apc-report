18c39717a6595aad4863e49afa0cb2264a5fe573
lucas-robin <lucas.rob1@live.fr>
2020-07-13 18:31:48 +0200

    [CHANGE] Optimize report factory with faker data

--- database/factories/ReportFactory.php
+++ database/factories/ReportFactory.php
@@ -5,10 +5,35 @@
 use App\Report;
 use Faker\Generator as Faker;
 
-$factory->define(Report::class, function (Faker $faker) {
+function generateSurvey(Faker $faker): string {
+    $survey = [
+        'profile' => $faker->randomElement(['agency', 'firm']),
+        'reception' => $faker->randomElement([1, 2, 3]),
+        'subject' => $faker->randomElement(['new_partner', 'routine_visit']),
+        'has_described_society' => $faker->boolean(),
+        'has_showed_extranet' => $faker->boolean(),
+        'has_showed_automotive_pricing' =>  $faker->boolean(),
+        'has_distributed_visual_support' =>  $faker->boolean(),
+        'appointment_quality' => $faker->randomElement([1, 2, 3]),
+        'products_evoked' => [
+            'auto' =>  $faker->boolean(),
+            'housing' =>  $faker->boolean(),
+            'moto' =>  $faker->boolean(),
+            'health' =>  $faker->boolean(),
+            'juridic' =>  $faker->boolean(),
+            'life' =>  $faker->boolean(),
+        ],
+        'monthly_volume' => $faker->randomElement([1, 2, 3, 4]),
+    ];
+
+    return json_encode($survey);
+}
+
+$factory->define(Report::class, function(Faker $faker) {
     return [
+        'appointment_id' => $faker->numberBetween(1, 10),
         'user_id' => $faker->numberBetween(1, 10),
         'customer_id' => $faker->numberBetween(1, 10),
-        'content' => $faker->text,
+        'survey' => generateSurvey($faker),
     ];
 });
