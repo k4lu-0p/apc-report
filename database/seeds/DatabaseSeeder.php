aa681b2f2141f3fe259d6cada3f6d7a17e7245b1
lucas-robin <lucas.rob1@live.fr>
2020-07-13 18:30:47 +0200

    [FEAT] Join seeds in one seeder file

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,6 +12,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        $this->call([
+            UserSeeder::class,
+            AppointmentSeeder::class,
+            ReportSeeder::class,
+            CustomerSeeder::class,
+        ]);
     }
 }
