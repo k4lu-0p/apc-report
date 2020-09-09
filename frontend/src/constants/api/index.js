1a3f67459d4337633b4bb9dab46098cfc1f0baf0
lucaslocal <lucaslocal@PCDIR06.EURALPHA.local>
2020-09-09 13:51:21 +0200

    [FIX] constants

--- frontend/src/constants/api/index.js
+++ frontend/src/constants/api/index.js
@@ -5,8 +5,8 @@ import STATUS from './status';
 
 export default {
   // BASE_URL: '', // Dev mod
-  // BASE_URL: 'http://localhost:8000', // Vue Serve mod
-  BASE_URL: 'https://apc-report.lucasrobin.fr', // Preprod mod
+  BASE_URL: 'http://localhost:8000', // Vue Serve mod
+  // BASE_URL: 'https://apc-report.lucasrobin.fr', // Preprod mod
   ENDPOINTS: {
     APC_REPORT,
     OPEN_WEATHER_MAP,
